36b707064ffd4082c1b647fd4d306c55172845d8
ahne2025 <645352528@qq.com>
2025-03-28 12:11:11 +0800

    Update make_befor.sh

--- REDMI_AX6000/make_befor.sh
+++ REDMI_AX6000/make_befor.sh
@@ -11,6 +11,4 @@
 #
 
 # Custom for REDMI AX6000
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-
-
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/luci2/bin/config_generate
