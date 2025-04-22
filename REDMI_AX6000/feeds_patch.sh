94df22e0c29788db40a1ecb5ad0e964a3b64c1f9
ahne2025 <645352528@qq.com>
2025-04-22 13:14:50 +0800

    Update feeds_patch.sh

--- REDMI_AX6000/feeds_patch.sh
+++ REDMI_AX6000/feeds_patch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 ./scripts/feeds update -a
+./scripts/feeds update -a
 rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 ./scripts/feeds install -a
