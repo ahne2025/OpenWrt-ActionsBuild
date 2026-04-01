5ffd387f4148a06037166ebc3558e84fedc8fb31
ahne2025 <645352528@qq.com>
2026-04-01 14:10:38 +0800

    Update make_befor.sh

--- REDMI_AX6000/make_befor.sh
+++ REDMI_AX6000/make_befor.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/luci/bin/config_generate
 ##任务设置调到系统菜单
 # sed -i '2,8d' package/feeds/jell/luci-app-taskplan/root/usr/share/luci/menu.d/luci-app-taskplan.json
-sed -i '3,7d' package/feeds/taskplan/luci-app-taskplan/root/usr/share/luci/menu.d/luci-app-taskplan.json
+sed -i '2,8d' package/feeds/taskplan/luci-app-taskplan/root/usr/share/luci/menu.d/luci-app-taskplan.json
 # sed -i '/firstchild/d' package/feeds/jell/luci-app-taskplan/luasrc/controller/*.lua
-# sed -i 's/"control"/"system"/g' package/feeds/jell/luci-app-taskplan/root/usr/share/luci/menu.d/luci-app-taskplan.json
-sed -i 's/"control"/"system"/g' package/feeds/taskplan/luci-app-taskplan/root/usr/share/luci/menu.d/luci-app-taskplan.json
+# sed -i 's/control/system/g' package/feeds/jell/luci-app-taskplan/root/usr/share/luci/menu.d/luci-app-taskplan.json
+sed -i 's/control/system/g' package/feeds/taskplan/luci-app-taskplan/root/usr/share/luci/menu.d/luci-app-taskplan.json
